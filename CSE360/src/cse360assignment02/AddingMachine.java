15b33cf870dcbad86c4ebd1d4df26859ede7dad5
sid <sid@localhost>
2020-10-06 16:38:45 -0700

    the initial version

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+// Name: Siddharth Agarwal
+// class: CSE 360 
+// Assignment:1 
+
+package cse360assignment02;
+
+class AddingMachine
+
+{
+	 private int total;
+	  
+	  public AddingMachine () 
+	  {
+	    total = 0; // not needed - included for clarity
+	  }
+	  // method to get total
+	  public int getTotal () 
+	  { 
+	    return 0;
+	  }
+	  //method to add
+	  public void add (int value) 
+	  {
+		  
+	  }
+	  //method to subtract
+	  public void subtract (int value) 
+	  { 
+		  
+	  }
+	  // method toString return the result at last
+	  public String toString () 
+	  { 
+	    return "";
+	  }
+	  // method to clear 
+	  public void clear() 
+	  { 
+		  
+	  }
+
+}
